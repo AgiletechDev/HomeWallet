e940de4b0a9343d17fa6a7928189d25156c3ea27
Grober Gonzales <GonzalesDLCGrober@gmail.com>
2024-05-31 16:52:19 -0500

    roadmap

--- src/views/roadmap/FirstSection.jsx
+++ src/views/roadmap/FirstSection.jsx
@@ -1,9 +1,5 @@
-import { Box, Typography, useTheme, useMediaQuery } from '@mui/material'
-import lineahorizonta1 from '../../assets/images/portal/lineahorizonta1.png'
+import { Box, Typography, useTheme, useMediaQuery, Stack } from '@mui/material'
 import roadmap from '../../assets/images/portal/roadmap.png'
-import fondo1Roadmap from '../../assets/images/portal/fondo1Roadmap.png'
-import apoyo1 from '../../assets/images/portal/apoyo1.png'
-import apoyo2 from '../../assets/images/portal/apoyo2.png'
 
 export const FirstSection = () => {
   const theme = useTheme()
@@ -13,35 +9,14 @@ export const FirstSection = () => {
       sx={{
         display: 'flex',
         height: isMobile ? '100%' : '900px',
-        width: isMobile ? '100%' :  '99vw',
-        overflow: 'hidden',
+        width: '100%',
         position: 'relative',
       }}
     >
-      {!isMobile && (
-        <>
-          <img
-            src={apoyo1}
-            alt="circulo"
-            height="100px"
-            width="80px"
-            style={{ position: 'absolute', left: '12%', top: '10%' }}
-          />
-          <img
-            src={apoyo1}
-            alt="circulo"
-            height="100px"
-            width="80px"
-            style={{ position: 'absolute', left: '6%', top: '19%' }}
-          />
-        </>
-      )}
-
-      <Box
+      <Stack
+        alignItems='center'
+        gap={8}
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
           marginTop: '40px',
           height: isMobile ? '100%' : '1000px',
           width: '99vw',
@@ -49,37 +24,26 @@ export const FirstSection = () => {
         }}
       >
         <Typography
+          variant='h3'
+          fontWeight='900'
+          fontSize='80px'
+          top={0}
+          className='absolute text-white text-shadow -z-10'
+        >
+          ROADMAP
+        </Typography>
+        <Typography
           variant="h5"
           sx={{
             fontSize: '35px',
             fontWeight: '600',
             marginTop: '20px',
-            color: '#33277b',
           }}
         >
           ROADMAP
         </Typography>
-        <img src={lineahorizonta1} alt="" width="15%" />
         <img src={roadmap} alt="" width="1000px" />
-        {!isMobile && (
-          <img src={fondo1Roadmap} alt="" width="100%" />
-        )}
-       
-      </Box>
-      {!isMobile && (
-      <img
-        src={apoyo2}
-        alt="lineas"
-        width="400px"
-        height="300px"
-        style={{
-          position: 'absolute',
-          top: '20%',
-          right: '-220px',
-          zIndex: '-1',
-        }}
-      />
-      )}
+      </Stack>
     </Box>
   )
 }
--- src/views/smartContract/FirstSection.jsx
+++ src/views/smartContract/FirstSection.jsx
@@ -1,12 +1,8 @@
-import React from 'react'
-import { Box, Button, Typography, useTheme, useMediaQuery } from '@mui/material'
+import { Box, Button, Typography, useTheme, useMediaQuery, Stack } from '@mui/material'
 import solidprooficon from '../../assets/images/portal/solidprooficon.png'
-import cuadro1 from '../../assets/images/portal/cuadro1.png'
-import lineahorizonta1 from '../../assets/images/portal/lineahorizonta1.png'
-import apoyo2 from '../../assets/images/portal/apoyo2.png'
-import apoyo2_1 from '../../assets/images/portal/apoyo2_1.png'
-import apoyo4 from '../../assets/images/portal/apoyo4.png'
-import circulos from '../../assets/images/portal/circulos.png'
+import icon1 from '../../assets/images/portal/smart/image-1.png'
+import icon2 from '../../assets/images/portal/smart/image-2.png'
+import icon3 from '../../assets/images/portal/smart/image-3.png'
 import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 
@@ -38,51 +34,42 @@ export const FirstSection = () => {
   return (
     <Box
       sx={{
+        position: 'relative',
         display: 'flex',
+        width: '100%',
       }}
     >
-      {!isMobile && (
-        <>
-          <img
-            src={apoyo2}
-            alt="lineas"
-            width="400px"
-            height="300px"
-            style={{ position: 'absolute', top: '50%', left: '-200px' }}
-          />
-          <img
-            src={apoyo2_1}
-            alt="lineas"
-            width="500px"
-            height="600px"
-            style={{ position: 'absolute', top: '55%', left: '0', zIndex: -1 }}
-          />
-        </>
-      )}
-
       <Box
         sx={{
+          position: 'relative',
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
           height: isMobile ? '100%' : '900px',
           marginTop: '40px',
-          width: isMobile ? '100%' : '99vw',
+          width: '100%',
         }}
       >
+        <Typography
+          variant='h3'
+          fontWeight='900'
+          fontSize='80px'
+          top={-40}
+          className='absolute text-white text-shadow -z-10'
+        >
+          {t("TITLE_SMARTCONTRACT")}
+        </Typography>
         <Typography
           variant="h5"
           sx={{
             fontSize: '35px',
             fontWeight: '600',
             marginTop: '20px',
-            color: '#33277b',
             textAlign: 'center',
           }}
         >
           {t('TITLE_SMARTCONTRACT')}
         </Typography>
-        <img src={lineahorizonta1} alt="" width="25%" />
         <Box
           sx={{
             display: 'flex',
@@ -97,7 +84,7 @@ export const FirstSection = () => {
                 variant="h6"
                 sx={{
                   fontWeight: '600',
-                  color: '#33277b',
+                  color: '#0A85D9',
                   mt: '20px',
                 }}
               >
@@ -111,7 +98,6 @@ export const FirstSection = () => {
                   fontSize: '20px',
                   fontWeight: '600',
                   textDecoration: 'none',
-                  color: '#33277b',
                 }}
               >
                 {shortenUrl(url, isMobile)}
@@ -123,16 +109,20 @@ export const FirstSection = () => {
             sx={{
               fontSize: '15px',
               fontWeight: '600',
-              color: '#33277b',
+              color: '#0A85D9',
               mt: '10px',
               mb: '10px',
             }}
           >
             {t('SUBTITLE3')}
           </Typography>
+          <a href='https://github.com/solidproof/projects/tree/main/Wallet%20Token'>
+            https://github.com/solidproof/projects/tree/main/Wallet%20Token
+          </a>
           <img
             src={solidprooficon}
             alt="img solidProof"
+            className='saturate-0 brightness-50'
             style={{ height: '100px', marginTop: '10px', marginBottom: '5px' }}
           />
           <Typography
@@ -142,7 +132,6 @@ export const FirstSection = () => {
               fontWeight: '800',
               marginTop: '20px',
               marginBottom: '25px',
-              color: '#33277b',
             }}
           >
             {t('SUBTITLE4')}
@@ -157,21 +146,26 @@ export const FirstSection = () => {
               maxWidth: '700px',
             }}
           >
-            <img src={cuadro1} alt="" style={{ marginRight: '10px' }} />
-            <img src={cuadro1} alt="" style={{ marginRight: '10px' }} />
-            <img src={cuadro1} alt="" style={{ marginRight: '10px' }} />
+            {
+              [icon1, icon2, icon3].map((src, index) => (
+                <Stack key={index} gap={2}>
+                  <img src={src} width={328} height={98} style={{ marginRight: '10px' }} />
+                  <Box className='h-1 my-1 rounded-lg bg-black' />
+                </Stack>
+              ))
+            }
           </Box>
           <Link to='/buywallets'>
             <Button
               sx={{
-                background: '#33277b',
-                borderRadius: '20px',
-                marginTop: '50px',
+                background: '#0A85D9',
+                borderRadius: '40px',
+                paddingX: '30px',
+                marginY: '50px',
                 fontWeight: '600',
                 fontSize: '25px',
                 color: 'white',
                 textTransform: 'capitalize',
-                mb:'2em'
               }}
             >
               {t('BUTTONNAME')}
@@ -179,25 +173,6 @@ export const FirstSection = () => {
           </Link>
         </Box>
       </Box>
-
-      {!isMobile && (
-        <>
-          <img
-            src={circulos}
-            alt="lineas"
-            width="100px"
-            height="100px"
-            style={{ position: 'absolute', bottom: '0%', right: '1%' }}
-          />
-          <img
-            src={apoyo4}
-            alt="lineas"
-            width="500px"
-            height="600px"
-            style={{ position: 'absolute', top: '55%', right: '0', zIndex: -1 }}
-          />
-        </>
-      )}
     </Box>
   )
 }
